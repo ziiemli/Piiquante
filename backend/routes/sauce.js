0836ea5f05d190630e00c57f785fb001cf290260
ziiemli <zilin.moulin@outlook.fr>
2022-10-21 19:28:18 +0200

    clean

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -12,8 +12,8 @@ const sauceCtrl = require("../controllers/sauce")
 
 //actions
 router.get("/", auth, sauceCtrl.getAllSauces)
-router.post("/", auth, multer, sauceCtrl.createSauce)
 router.get("/:id", auth, sauceCtrl.getOneSauce)
+router.post("/", auth, multer, sauceCtrl.createSauce)
 router.put("/:id", auth, multer, sauceCtrl.modifySauce)
 router.delete("/:id", auth, sauceCtrl.deleteSauce)
 router.post('/:id/like', auth, sauceCtrl.likeSauce)
