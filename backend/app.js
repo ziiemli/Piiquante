f8a8fb6d427cea5c170ee0ed1daf085eeb683339
ziiemli <zilin.moulin@outlook.fr>
2022-10-27 17:06:27 +0200

    clean code

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,7 @@
 //import express
 const express = require("express")
-const formData = require("express-form-data")
 const app = express()
-//import package to use environment variables
+//import dotenv to use environment variables
 const dotenv = require("dotenv").config()
 
 //access to the request body
@@ -20,7 +19,7 @@ const sauceRoutes = require("./routes/sauce")
 //mongoose
 mongoose
     .connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => console.log("Connexion à MZongoDB réussie !"))
+    .then(() => console.log("Connexion à MongoDB réussie !"))
     .catch(() => console.log("Connexion à MongoDB échouée !"))
 
 //allow software to access API
